bbbac6cf4ef341cf52fe901d739470d6ad6a16e6
JeisonOlaya <75368622+JeisonOlaya@users.noreply.github.com>
2025-04-25 20:15:48 -0500

    Add files via upload

--- modi.py
+++ modi.py
@@ -4,17 +4,20 @@ print("*** ESTA ES TU FACTURA DE VENTA ***")
 print("**"*18)
 print("\n")
 
+# en la linea 8 creamos la lista
+
 user= []
 proceso = True
+# inicia un while que esta corriendo por todo el programa para que el usurio pueda repetir el proceso si desea agregar otro producto.
 
-while proceso:
+while proceso:    
     nombre = input("Por favor ingresar el nombre del producto: ")
 
     #Aca estamos validando que el precio del producto sea positivo
     valPre = True
     while valPre:
         try:
-            precio= int (input("Ingresa el precio del producto: ")) 
+            precio= float (input("Ingresa el precio del producto: ")) 
             if precio<=0:
                 print("el Dato ingresado del precio es erroneo")
                     
@@ -34,6 +37,8 @@ while proceso:
                 valCant=False
         except:
             print("Intentaste escribir una letra/palabra")
+        
+    user.append([cantidad,nombre, precio])
 
     #Aca estamos validando el porcentaje de descuento
     valDesc=True
@@ -47,18 +52,35 @@ while proceso:
         except:
             print("Intentaste escribir una letra/palabra")
 
-    totalSinDesc = cantidad*precio
-    descuentoCompra = (totalSinDesc*descuento)/100
-    compraTotal = (totalSinDesc-descuentoCompra)
+
 
     volver =int(input("deseas agregar otro producto a la lista? digita 1 para Si o 2 para No: "))
 
     if volver == 2:
-        print("Fin de la compra")
+        print("Fin de la compra \n")        
         proceso = False
 
-    
-print("El total de la compra es:")
+
+
+contador = 0
+print("El total de la compra sin descuento es: \n")
 print("cantidad, nombre, precio")
-print(f"{cantidad}          {nombre}     {totalSinDesc:.2f}")
-print(f"El total de la compra con descuento es: \n{cantidad}          {nombre}     {compraTotal:.2f}")
+c = 0
+
+
+total = 0
+totDesc= 0
+for product in user:
+       
+    total = total + (product[0] * product[2])
+    descuentoCompra = (total*descuento)/100
+    totDesc = (total-descuentoCompra)
+
+for i in user:
+    print(i[0], i[1], i[2])
+
+print(f"total= {total:.2f}\n")
+print("El total de la compra con descuento es: \n")
+print("===============================")
+print(f"total= {totDesc:.2f}")
+
